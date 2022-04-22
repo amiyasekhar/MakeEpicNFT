07c6425681f0ef9229c765373d3e97ed967a2e83
amiyasekhar <amiyasekhar@gmail.com>
2022-04-22 16:33:48 -0400

    NC10-150 continuing work

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -16,15 +15,9 @@ contract MyEpicNFT is ERC721URIStorage, Ownable{
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
   address payable _reciever;
-  uint256 private _maxSupply = 15;
+  uint256 private _maxSupply = 420;
   uint256 private royalty = 25;
-  address AmiyaAddress = 0x5d1819D05B58d5967520f3d881A36f212C02915B;
-  address AbbyAddress = 0xc51573625b845826Bc3f98f2191AEa6b17Cde013;
-  uint256 percentage;
-  uint256 amount;
-
   event NewEpicNFTMinted(address sender, uint256 tokenId);
-
 
   constructor() ERC721 ("Custom NFT", "NFT") payable {
     console.log("This is my NFT contract. Woah!");
@@ -39,7 +32,6 @@ contract MyEpicNFT is ERC721URIStorage, Ownable{
     console.log(balanceOf(msg.sender), "sender balance");
     console.log(msg.value, "message sender value");
     console.log(gasleft(), "gas fee");
-    require(_tokenIds.current()+1 <= 420, "We're sold out, wait for the next collection!");
     require(msg.value >= 0.2 ether, "Need to send 0.2 ether or more");
     _safeMint(msg.sender, newItemId);
     _setTokenURI(newItemId, "https://jsonkeeper.com/b/TR0K");
@@ -58,24 +50,17 @@ contract MyEpicNFT is ERC721URIStorage, Ownable{
 
   function returnTokenID() public view returns (uint256){
     return _tokenIds.current();
-  }
-
-  function setPercent(uint256 per) public{
-    percentage = per;
-  }
-  function set  
+  }  
 
   function sendBalanceToAddress() internal{
-
+    address reciever = 0xc51573625b845826Bc3f98f2191AEa6b17Cde013;
+    uint256 amount = 0.2 ether;
     require(
         amount <= address(this).balance,
         "Trying to withdraw more money than the contract has."
     );
-    uint myCut = amount * percentage/100 ether;
-    (bool AmiyaSuccess,) = (AmiyaAddress).call{value: myCut}("");
-    require(AmiyaSuccess, "Failed to withdraw money from contract.");    
-    (bool AbbySuccess,) = (AbbyAddress).call{value: amount - myCut}("");
-    require(AbbySuccess, "Failed to withdraw money from contract.");
+    (bool success,) = (reciever).call{value: amount}("");
+    require(success, "Failed to withdraw money from contract.");
   }
 
 /*{
@@ -83,7 +68,6 @@ contract MyEpicNFT is ERC721URIStorage, Ownable{
     "description": "A beach with a view",
     "image": "https://i.imgur.com/VWPBjl4.jpeg"
 }
-
 https://jsonkeeper.com/b/TR0K*/
 
 
