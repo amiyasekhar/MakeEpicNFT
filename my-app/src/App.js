dc3130921e8437ce4cf749dfd0526267a9746caf
amiyasekhar <amiyasekhar@gmail.com>
2022-07-02 22:29:34 -0400

    able to connect using default provider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import { ethers } from "ethers";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
-  const CONTRACT_ADDRESS = "0x1B95B9b497F7F3F218461Bcf27D0833d8b842DfF";
+  const CONTRACT_ADDRESS = "0xCd3194fC20ACe6D514F1596607031588171a9B5d";
   const [supply, setSupply] = useState('');
   const [price, setPrice] = useState('');
   const [hash, setHash] = useState('');
@@ -15,6 +15,10 @@ const App = () => {
   const [raribleLink, setRaribleLink] = useState("https://rarible.com/");
   const [etherscanLink, setEtherscanLink] = useState("https://etherscan.io/tx/")
   const [token, setToken] = useState(null);
+  const [displayText, setDisplayText] = useState(`Supply: ${0}, Price: ${0} ETH, Hash: `);
+  let globalProvider = null;
+  let globalSigner = null;
+  let globalConnectedContract = null;
   console.log("default rarible link", raribleLink);
   console.log("default opensea link", openSeaLink);
 
@@ -22,12 +26,34 @@ const App = () => {
     const { ethereum } = window;
 
     if (!ethereum) {
+      const provider = new ethers.getDefaultProvider('https://eth-rinkeby.alchemyapi.io/v2/Ky5-_i9Lkd1buVYM5zxMymn-SShw4023');
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, provider);
+      globalProvider = provider;
+      globalConnectedContract = connectedContract;
+      console.log(globalProvider, "def provider");
+      console.log(globalConnectedContract, "contract from def provider");
       console.log("Make sure you have metamask!");
       return;
     } else {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+      globalProvider = provider;
+      globalSigner = signer;
+      globalConnectedContract = connectedContract;
+      console.log(globalProvider, "eth provider");
+      console.log(globalSigner, "signer from eth provider");
+      console.log(globalConnectedContract, "contract from eth provider");
       console.log("We have the ethereum object", ethereum);
-    }
 
+    }
+    let outstandingSupply = await globalConnectedContract.getCurrentSupply();
+    let cost = await globalConnectedContract.getPrice();
+    setPrice(cost);
+    setSupply(outstandingSupply.toNumber())
+    console.log(outstandingSupply.toNumber(), "This is outstanding supply");
+    console.log(ethers.utils.formatEther(cost), "the price");
+    setDisplayText(`Supply: ${outstandingSupply.toNumber()}, Price: ${ethers.utils.formatEther(cost)}, Hash: ${hash}`);
     const accounts = await ethereum.request({ method: 'eth_accounts' });
 
     if (accounts.length !== 0) {
@@ -87,10 +113,18 @@ const App = () => {
       const { ethereum } = window;
   
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
+        if (globalSigner == null){
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+          globalProvider = provider;
+          globalSigner = signer;
+          globalConnectedContract = connectedContract;
+        }
+        /*const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
-        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);*/
+        globalConnectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber())
           alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: 
           \nhttps://rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}
@@ -102,13 +136,14 @@ const App = () => {
           setToken(tokenId)
         });
         console.log("Going to pop wallet now to pay gas...")
-        let outstandingSupply = await connectedContract.getCurrentSupply();
-        setSupply(outstandingSupply.toNumber() + "; ")
+        let outstandingSupply = await globalConnectedContract.getCurrentSupply();
+        setSupply(outstandingSupply.toNumber())
         console.log("This is outstanding supply", outstandingSupply.toNumber())
-        let cost = await connectedContract.getPrice();
-        setPrice(ethers.utils.formatEther(cost) + "ETH; ")
-        console.log("This is cost", ethers.utils.formatEther(cost))
-        let nftTxn = await connectedContract.makeAnEpicNFT({value: ethers.utils.parseEther(ethers.utils.formatEther(cost)), gasLimit: 10000000});
+        let cost = await globalConnectedContract.getPrice();
+        setPrice(cost);
+        console.log("This is cost", ethers.utils.formatEther(cost));
+        const artistAddresses = ["0x3cB1DE1465310F5fAD3C65A58F1b174b78D15E71", "0x57987efdC232231510DF7d1ea3f223Dd69Df3BEa"];
+        let nftTxn = await globalConnectedContract.makeAnEpicNFT(artistAddresses, {value: ethers.utils.parseEther(ethers.utils.formatEther(price)), gasLimit: 10000000});
   
         console.log("Mining...please wait.")
         await nftTxn.wait();
@@ -160,9 +195,7 @@ const App = () => {
               First 100 NFTs will mint for 0.12 ETH, the next 100 will mint for 0.19 ETH. the last 200 will mint for 0.5 ETH. Click "Mint" to see the price and supply, after which you may choose to purchase an NFT.
             </p>
             <p className={"mini-text"}>
-              Price before mint: {price}
-              Supply before mint: {supply}
-              Hash: {hash}
+              {displayText}
             </p>
             {currentAccount === "" ? (
               renderNotConnectedContainer()
